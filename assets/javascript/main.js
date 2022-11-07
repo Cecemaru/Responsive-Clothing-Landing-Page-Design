c3f5e932568a0de4f812833bf6f50d029c230893
Ömer Düzgün <Cecemaru@hotmail.com>
2022-11-07 13:26:30 +0300

    footer section and scrollup button add

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -37,3 +37,13 @@ let swiperProduct = new Swiper(".products__container", {
     },
   },
 });
+
+// ================= SHOW HIDE SCROLL UP ===============
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
