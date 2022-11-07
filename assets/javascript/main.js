ac1ad78fc3b83b4ec02da591360992b02bdd5d22
Ömer Düzgün <Cecemaru@hotmail.com>
2022-11-07 11:23:39 +0300

    products section and swiper library add. Swiper breakepoint as javascript change.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,3 +7,33 @@ const scrollHeader = () => {
     : header.classList.remove("scroll-header");
 };
 window.addEventListener("scroll", scrollHeader);
+
+// ==================== SWIPER PRODUCT ==================
+let swiperProduct = new Swiper(".products__container", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      spaceBetween: 72,
+    },
+  },
+});
